2f69955ed4af7723eec259ee1150732c088bc020
Ritik Raj <ritik@air-2.local>
2025-10-09 01:38:26 +0530

    Implement ECHO command

--- app/main.go
+++ app/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -13,21 +14,12 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	// fmt.Println("Logs from your program will appear here!")
-
 	// Start listening on TCP port 6379 on all interfaces.
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	// conn, err := l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
-	// conn.Write([]byte("+PONG\r\n"))
 
 	for {
 		// Wait for a connection.
@@ -49,14 +41,86 @@ func handleConnection(conn net.Conn) {
 	fmt.Println("New client connected:", conn.RemoteAddr().String())
 	for {
 		// Read a line from the connection.
-		text, err := reader.ReadString('\n')
+		args, err := readRESP(reader)
 		if err != nil {
 			fmt.Println("Error reading from connection: ", err.Error())
+			writeError(conn, "Error reading input")
 			return
 		}
 
-		if strings.TrimSpace(text) == "PING" {
-			conn.Write([]byte("+PONG\r\n"))
+		if len(args) == 0 {
+			continue
+		}
+
+		cmd := strings.ToUpper(args[0])
+
+		switch cmd {
+		case "PING":
+			writeSimpleString(conn, "PONG")
+		case "ECHO":
+			if len(args) < 2 {
+				writeError(conn, "ECHO requires an argument")
+			} else {
+				writeBulkString(conn, args[1])
+			}
+		default:
+			writeError(conn, "Unknown command: "+cmd)
 		}
 	}
+}
+
+func readRESP(reader *bufio.Reader) ([]string, error) {
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+
+	line = strings.TrimSpace(line)
+	if len(line) == 0 || line[0] != '*' {
+		return nil, nil
+	}
+
+	numArgs, err := strconv.Atoi(line[1:])
+	if err != nil {
+		return nil, err
+	}
+
+	args := make([]string, 0, numArgs)
+	for i := 0; i < numArgs; i++ {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+
+		line = strings.TrimSpace(line)
+		if len(line) == 0 || line[0] != '$' {
+			return nil, nil
+		}
+
+		argLen, err := strconv.Atoi(line[1:])
+		if err != nil {
+			return nil, err
+		}
+
+		arg := make([]byte, argLen+2) // +2 for \r\n
+		_, err = reader.Read(arg)
+		if err != nil {
+			return nil, err
+		}
+
+		args = append(args, string(arg[:argLen]))
+	}
+	return args, nil
+}
+
+func writeSimpleString(conn net.Conn, message string) {
+	conn.Write([]byte("+" + message + "\r\n"))
+}
+
+func writeBulkString(conn net.Conn, message string) {
+	conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(message), message)))
+}
+
+func writeError(conn net.Conn, message string) {
+	conn.Write([]byte("-" + message + "\r\n"))
 }
